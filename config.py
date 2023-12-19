f67bc9bb7fa928de9123cc53a9f74a09f8333b83
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2023-12-19 03:08:49 -0300

    adicionando user do gmail no arquivo de configuração

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 pass_gmail = ... # ------------- COLOQUE SUA SENHA DE APLICATIVO DO GMAIL-----------------
+user_gmail = ... # ------------- COLOQUE SEU USER DO GMAIL -------------------------------
