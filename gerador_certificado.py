f9e5716bf0f69ea45b11e4459ab8656a137048eb
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2023-12-19 02:08:05 -0300

    centralizando textos do certificado dinamicamente

--- gerador_certificado.py
+++ gerador_certificado.py
@@ -19,16 +19,30 @@ class GeradorCertificado:
         # Abrir a imagem de modelo
         imagem = Image.open('certificado.png')
         
-        texto = f'''Certificamos que {nome}, portador(a) do CPF {cpf}, 
-                participou da SECCOM 2023 entre os dias 6 de novembro a 10 de novembro,
-                totalizando {horas} horas.'''
-
+        texto1 = f'''Certificamos que {nome}, portador(a) do CPF {cpf},'''
+        texto2 = f'''participou da SECCOM 2023 entre os dias 6 de novembro a 10 de novembro,'''
+        texto3 = f'''totalizando {horas} horas de atividades.'''        
+        
         draw = ImageDraw.Draw(imagem)
+        fonte = ImageFont.truetype('arial.ttf', 35)
 
-        fonte = ImageFont.truetype('calibrib.ttf', 40)
+        # Centralizando o textos
+        largura_texto1, _ = draw.textsize(texto1, font=fonte)
+        largura_imagem, _ = imagem.size
+        ponto_inicio_texto1 = (largura_imagem - largura_texto1) / 2
 
-        # Definindo a cor da fonte como preto (0, 0, 0)
-        draw.text((310, 710), texto, font=fonte, fill=(0, 0, 0), align='center')
+        # Centralizando o texto2
+        largura_texto2, _ = draw.textsize(texto2, font=fonte)
+        ponto_inicio_texto2 = (largura_imagem - largura_texto2) / 2
+        
+        # Centralizando o texto3
+        largura_texto3, _ = draw.textsize(texto3, font=fonte)
+        ponto_inicio_texto3 = (largura_imagem - largura_texto3) / 2
+
+        # Definindo a cor da fonte como preto (0, 0, 0) e a posição dos textos 
+        draw.text((ponto_inicio_texto1, 710), texto1, font=fonte, fill=(0, 0, 0), align='center')
+        draw.text((ponto_inicio_texto2, 750), texto2, font=fonte, fill=(0, 0, 0), align='center')
+        draw.text((ponto_inicio_texto3, 790), texto3, font=fonte, fill=(0, 0, 0), align='center')
 
         imagem_certificado = f'{nome}_Certificado.png'
         imagem.save(imagem_certificado)
@@ -38,7 +52,7 @@ class GeradorCertificado:
     @staticmethod
     def enviar_email(nome, email, imagem_certificado):
         # Inicializar o Yagmail SMTP
-        usuario = yagmail.SMTP(user='seu_user@gmail.com', password=pass_gmail)
+        usuario = yagmail.SMTP(user='seu_user@gmail.com', password=pass_gmail) # Insira o email remetente 
         
         assunto = 'Certificado de Participação - SECCOM'
         conteudo = f'Olá {nome},\n\nAqui está o seu certificado de participação da SECCOM 2023. \n\nAtenciosamente,\nOrganização da SECCOM'
@@ -52,5 +66,5 @@ class GeradorCertificado:
         )
 
         print(f'Email enviado para {email} com sucesso!')
-        os.remove(imagem_certificado) # remove a imagem do diretório após enviar o email
+        os.remove(imagem_certificado) # Remove a imagem do diretório após enviar o email
         print(f"Certificado '{imagem_certificado}' excluído com sucesso.")
