54565c1e1eac8114bd9920a11ee4f1cb850ce7ac
Gustavo Nunes Viana <nunesvianagustavo@gmail.com>
2024-11-03 18:48:43 -0300

    csv

--- gerador_certificado.py
+++ gerador_certificado.py
@@ -3,14 +3,11 @@ import yagmail
 from config import pass_gmail, user_gmail
 import os
 
-def enviar_email_com_certificado(nome, cpf, horas, email): 
-        
-        cpf_formatado = formatar_cpf(cpf)
-        # Gerar um certificado com o nome, cpf e horas
-        imagem_certificado = criar_certificado(nome, cpf_formatado, horas)
 
-        # Enviar email personalizado
-        enviar_email(nome, email, imagem_certificado)
+def enviar_email_com_certificado(nome, cpf, horas, email):
+    cpf_formatado = formatar_cpf(cpf)
+    imagem_certificado = criar_certificado(nome, cpf_formatado, horas)
+    enviar_email(nome, email, imagem_certificado)
 
 
 def formatar_cpf(cpf):
@@ -19,13 +16,12 @@ def formatar_cpf(cpf):
 
 
 def criar_certificado(nome, cpf, horas):
-    # Abrir a imagem de modelo
     imagem = Image.open('assets/certificado.png')
-    
+
     texto1 = f'''Certificamos que {nome}, portador(a) do CPF {cpf},'''
     texto2 = f'''participou da SECCOM 2023 entre os dias 6 de novembro a 10 de novembro,'''
-    texto3 = f'''totalizando {horas} horas de atividades.'''        
-    
+    texto3 = f'''totalizando {horas} horas de atividades.'''
+
     draw = ImageDraw.Draw(imagem)
     fonte = ImageFont.truetype('assets/arial.ttf', 35)
 
@@ -36,14 +32,17 @@ def criar_certificado(nome, cpf, horas):
 
     largura_texto2, _ = draw.textsize(texto2, font=fonte)
     ponto_inicio_texto2 = (largura_imagem - largura_texto2) / 2
-    
+
     largura_texto3, _ = draw.textsize(texto3, font=fonte)
     ponto_inicio_texto3 = (largura_imagem - largura_texto3) / 2
 
-    # Definindo a cor da fonte como preto (0, 0, 0) e a altura dos textos 
-    draw.text((ponto_inicio_texto1, 710), texto1, font=fonte, fill=(0, 0, 0), align='center')
-    draw.text((ponto_inicio_texto2, 750), texto2, font=fonte, fill=(0, 0, 0), align='center')
-    draw.text((ponto_inicio_texto3, 790), texto3, font=fonte, fill=(0, 0, 0), align='center')
+    # Definindo a cor da fonte como preto (0, 0, 0) e a altura dos textos
+    draw.text((ponto_inicio_texto1, 710), texto1,
+              font=fonte, fill=(0, 0, 0), align='center')
+    draw.text((ponto_inicio_texto2, 750), texto2,
+              font=fonte, fill=(0, 0, 0), align='center')
+    draw.text((ponto_inicio_texto3, 790), texto3,
+              font=fonte, fill=(0, 0, 0), align='center')
 
     imagem_certificado = f'{nome}_Certificado.png'
     imagem.save(imagem_certificado)
@@ -51,13 +50,11 @@ def criar_certificado(nome, cpf, horas):
 
 
 def enviar_email(nome, email, imagem_certificado):
-    # Inicializar o Yagmail SMTP
-    usuario = yagmail.SMTP(user=user_gmail, password=pass_gmail) 
-    
+    usuario = yagmail.SMTP(user=user_gmail, password=pass_gmail)
+
     assunto = 'Certificado de Participação - SECCOM'
     conteudo = f'Olá {nome},\n\nAqui está o seu certificado de participação da SECCOM 2023. \n\nAtenciosamente,\nOrganização da SECCOM'
 
-    # Anexar a imagem do certificado
     usuario.send(
         to=email,
         subject=assunto,
@@ -66,5 +63,6 @@ def enviar_email(nome, email, imagem_certificado):
     )
 
     print(f'Email enviado para {email} com sucesso!')
-    os.remove(imagem_certificado) # Remove a imagem do diretório após enviar o email
+
+    os.remove(imagem_certificado)
     print(f"Certificado '{imagem_certificado}' excluído com sucesso.")
--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 import csv
 import unicodedata
 import gerador_certificado
-
-
-# ----------------- Dicionário com título da atividade e a quantidade de horas 
+# ----------------- Dicionário com título da atividade e a quantidade de horas
 # ----------------- Editar conforme as atividades requeridas
-# ----------------- É necessário remover as vírgulas dos nomes das atividades nos arquivos .csv 
-# ----------------- O nome da atividade aqui deve ser idêntico ao nome da atividade nos arquivos .csv 
-
+# ----------------- É necessário remover as vírgulas dos nomes das atividades nos arquivos .csv
+# ----------------- O nome da atividade aqui deve ser idêntico ao nome da atividade nos arquivos .csv
 palestras = {
     # Segunda
-    'Abertura SECCOM': 1,
-    'Palestra - Prevendo o desempenho de Alunos de Cursos Online com Deep Learning e Processamento de Linguagem Natural (Armindo Guerra)': 1,
-    'Painel - Perspectivas em IA (profs. Jerusa Marchi Mauro Roisenberg Jonata Tyska)': 2,
-    'Hackaton de Segurança': 4.5,
-    'Minicurso -  Introdução à Unity Game Engine': 3,
-    
+    'Palestra - Geração aleatória de objetos combinatórios para algoritmos criptográficos (Gustavo Zambonin)': 1,
+    'Palestra - Desafios do gerenciamento de identidade digital pelo usuário: Do uso inadequado de senhas à identidade digital descentralizada (Shirlei Chaves)': 1,
+    'Palestra - Sigilo sob uma perspectiva prioritariamente computacional (Wellington Silvano)': 1,
+    'Cerimônia de Abertura': 1,
+    'Palestra - PowerBI para profissionais de TI (Brian Henkels)': 1,
+    'Hackathon Flutter (Cainã Rinaldi Esteche e Renan Igor)': 5,
+    'KDE Neon & KDE Plasma: venha para o KDE e ganhe experiência profissional em TI (Mario Araujo Xavier)': 1,
+
     # Terça
-    'Palestra - Dados (semi não ...) estruturados: problemas e solução (#sqn) (Profa. Carina Dorneles)': 1,
-    'Palestra - Quem você pensa que é? Chris Bumstead CBum? Eu acho que não. Porque a pós-graduação precisa estar no seu radar de formação acadêmica (Profa. Patricia Plentz)': 1,
-    'Palestra - Tecnologia FPGA Intel no Mercado Brasileiro (Franciele Nornberg) - Macnica DHW': 1,
-    'Palestra - Perspectivas no uso de dados para Agricultura (Luiz Santana) - Leaf Agriculture': 1,
-    'Minicurso - Linux: comandos básicos (André Régis)': 3,
-    'Minicurso - Programação Linear Inteira (Profs. Álvaro Franco e Rafael de Santiago)': 1.5,
+    'Palestra - Passado, Presente e Futuro do Trabalho com Dados (Pâmela Nunes)': 1,
+    'Palestra - Oportunidades em Blockchain: Inovação e a Importância da Segurança (Pedro Veiga e Claudio Gerolimetto)': 1,
+    'Palestra - ADES: Assinador Digital com Certificados de Uso Único do LabSEC (Eduardo Perottoni)': 1,
+    'Painel Segurança (Dra. Carla Merkle Westphall, Dra. Thaís Bardini Idalino e Dr. Jean E. Martina)': 2,
+    'Palestra - FOSSHUB e o desafio de implantar ferramentas livres no mercado corporativo (Mario Araujo Xavier)': 1,
 
     # Quarta
-    'Minicurso - Desenvolvendo um aplicativo mobile com Flutter (Cainã Rinaldi Esteche)': 4,
-    'Palestra - Chips: Pilares da Era da Informação (prof. José Luís Güntzel)': 1.5,
-    'Maratona de Programação': 2.5,
-    'Roda de conversa sobre Computação & IA (PET Computação)': 1.5,
-    
+    'Palestra - Geocodificação em Python: Uma análise para os números de casas desocupadas em Florianópolis (Ale)': 1,
+    'Maratona de Programação': 1,
+    'Palestra - Segurança ofensiva: Hackeie sistemas e seja pago por isso (Roberto Rodrigues Filho e Derick Andrighetti)': 1,
+    'Palestra - Genesis: Segurança na Cloud by Design usando Infrastructure as Code (Luciano Borguetti Faustino)': 1,
+    'Palestra - Criptografia da Troca de Chaves: Clássica à Pós-Quântica (Matheus de Oliveira Saldanha)': 3.5,
+
     # Quinta
-    'Palestra - Introdução a programação de computadores quânticos - (Msc. Evandro Chagas da Rosa)': 1,
-    'Progressos e expectativas da computação quântica - Prof. Renato Portugal pesquisador Titular do LNCC/MCTI': 1,
-    'Minicurso - Introdução a geração de imagens com IA Gerativa (Prof. Aldo von Wangenheim/Thiago Luz)': 4,
-    'Palestra - Persistência Poliglota de Dados: Fundamentos e Oportunidades de Pesquisa  (Prof. Ronaldo Mello)': 1.5,
-    'Minicurso - Editor de Texto VIM (André Régis)': 3,
-    'Palestra - A IA no Desenvolvimento de Software no Mercado Livre  (Milton Bittencourt) -  Mercado Livre': 1.5,
+    'Palestra - Entre Bits e Clocks: Desvendando o Projetos de Circuitos Integrados (Rafael Oliveira)': 1,
+    'Palestra - Como entender vulnerabilidades: Do compilador a ReDoS + Live Hacking (Samuel Cardoso)': 1,
+    'Minicurso de LaTeX (Gustavo)': 1,
+    'Palestra - Assinatura digital tolerante a modificações (Anthony Kamers)': 1,
+    'Palestra - Tratamento de Dados JSON em Banco de Dados (Ronaldo dos Santos Mello)': 1.5,
+    'Palestra - Projetos Pessoais: De Hobby a Carreira Internacional (Mateus Cechetto)': 1,
+    'Palestra - FOSSHUB e o desafio de implantar ferramentas livres no mercado corporativo (Mario Araujo Xavier)': 1,
 
     # Sexta
-    'Palestra - Do Zero ao Hack: Hackeando Jogos de PS1 (Mateus Favarin Costa)': 1,
-    'Palestra - Microeletrônica é computação? Sim!!  (Profa. Cristina Meinhardt)': 1,
+    'Palestra - Proteção de dados no processo de desenvolvimento (Miliane Fantonelli)': 1,
+    'Dedos e Grafos: Vencendo com Matemática - Um guia para dominar Jogos de Estratégia (Cainã Rinaldi Esteche)': 1,
+    'Clean code ainda é Clean? (Gustavo)': 1,
+    'Trabalho Remoto em Empresas de Tecnologia (Ricardo Goes e Bruno Muniz)': 1,
+    'Hackathon Flutter - Premiação (Cainã Rinaldi Esteche e Renan Igor)': 1,
 }
 
 
@@ -54,7 +57,8 @@ def remover_acentos(texto):
 
 def encontrar_strings(string_grande, lista_strings_menores):
     # Filtra as strings menores encontradas na string grande
-    strings_encontradas = [substring for substring in lista_strings_menores if substring in string_grande]
+    strings_encontradas = [
+        substring for substring in lista_strings_menores if substring in string_grande]
 
     return strings_encontradas
 
@@ -63,7 +67,7 @@ def get_validos():
     validos = {}
     with open('info.csv', 'r') as arquivo:
         reader = csv.reader(arquivo)
-        
+
         for i, linha in enumerate(reader):
             email = linha[1]
             nome = linha[2]
@@ -80,12 +84,11 @@ def get_validos():
                     'palestras': [],
                     'horas': 0
                 }
-            
+
     return validos
 
 
 validos = get_validos()
-
 
 for dia in ['segunda', 'terca', 'quarta', 'quinta', 'sexta']:
     with open(f'{dia}.csv', 'r') as arquivo:
@@ -93,15 +96,15 @@ for dia in ['segunda', 'terca', 'quarta', 'quinta', 'sexta']:
         for i, linha in enumerate(reader):
             if i == 0:
                 continue
-            
+
             matricula = linha[2]
             string_grande = linha[3]
             lista_strings_menores = list(palestras.keys())
 
-
             if matricula in validos.keys():
-                validos[matricula]['palestras'] += encontrar_strings(string_grande, lista_strings_menores)
-    
+                validos[matricula]['palestras'] += encontrar_strings(
+                    string_grande, lista_strings_menores)
+
 
 chaves_a_remover = []
 
@@ -109,7 +112,7 @@ for participante in validos:
     total_horas = 0
     for palestra in validos[participante]['palestras']:
         total_horas += palestras[palestra]
-    
+
     # print(validos[participante]['palestras'])
     validos[participante]['horas'] = total_horas
 
@@ -123,4 +126,5 @@ for ausente in chaves_a_remover:
 
 # Classe que gera e envia os certificados por email
 for participante in validos:
-    gerador_certificado.enviar_email_com_certificado(validos[participante]['nome'], validos[participante]['cpf'], validos[participante]['horas'], validos[participante]['email'])
+    gerador_certificado.enviar_email_com_certificado(
+        validos[participante]['nome'], validos[participante]['cpf'], validos[participante]['horas'], validos[participante]['email'])
